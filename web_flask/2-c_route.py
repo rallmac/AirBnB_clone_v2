d8e2fb90652b7b5e1f0c5b9167c00ff9e7db5072
rallmac2 <root@tobijahdevice>
2024-07-28 22:49:02 +0100

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This script starts a flask web app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """
+    Default response
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Returns HBNB only
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_with_params(text):
+    """
+    c with params
+    """
+    text_no_underscore = text.replace('_', ' ')
+    return "C {}".format(text_no_underscore)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
